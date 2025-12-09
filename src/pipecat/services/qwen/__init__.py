c4f2b390057874022255b251e75b5ba9297b2ba9
ZIYI YANG <69795661+yangzy39@users.noreply.github.com>
2025-12-09 16:36:00 +0800

    Update __init__.py

--- src/pipecat/services/qwen/__init__.py
+++ src/pipecat/services/qwen/__init__.py
@@ -9,5 +9,8 @@ import sys
 from pipecat.services import DeprecatedModuleProxy
 
 from .llm import *
+from .stt import *
+from .tts import *
 
-sys.modules[__name__] = DeprecatedModuleProxy(globals(), "qwen", "qwen.llm")
+
+sys.modules[__name__] = DeprecatedModuleProxy(globals(), "qwen", "qwen.[llm,stt,tts]")
